b6f08f19182e30f28b11857d6acc9dc91c9c76da
agustinfernandez01 <agustinalejofernandez2001@gmail.com>
2026-02-24 16:39:28 -0300

    .

--- src/app/db.py
+++ src/app/db.py
@@ -34,7 +34,6 @@ if not DATABASE_URL:
     else:
         DATABASE_URL = "sqlite:///./local.db"
 
-<<<<<<< HEAD
 _connect_args = {}
 
 if DATABASE_URL.startswith("sqlite"):
@@ -47,14 +46,6 @@ elif DATABASE_URL.startswith("postgresql"):
     # psycopg2 no usa connect_timeout en connect_args
     _connect_args = {}
 
-=======
-if "sqlite" in DATABASE_URL:
-    _connect_args = {"check_same_thread": False}
-elif "postgresql" in DATABASE_URL:
-    _connect_args = {}
-else:
-    _connect_args = {"connect_timeout": 10}
->>>>>>> c68e60eec249b95d81c05e3ddb309e7faa71df16
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True,
