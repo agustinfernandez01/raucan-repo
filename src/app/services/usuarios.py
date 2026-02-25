55e640902299a70deb2aee1d9b7d67df7e0b81ca
agustinfernandez01 <agustinalejofernandez2001@gmail.com>
2026-02-25 19:42:05 -0300

    user

--- src/app/services/usuarios.py
+++ src/app/services/usuarios.py
@@ -55,7 +55,7 @@ def create_usuario(db: Session, usuario: UsuarioCreate):
         email=usuario.email,
         password_hash=hash_password(usuario.password), 
         telefono=usuario.telefono,
-        rol=usuario.rol,
+        rol="user",
         activo=usuario.activo,
         creado_en=datetime.now(),
     )
